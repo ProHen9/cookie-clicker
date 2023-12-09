2d4cd16811bc44462e1fb4df469a0a88de532574
Henri <henri.me@skiff.com>
2023-12-09 17:10:17 +0100

    add framer motion and minimalism

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # a minimal cookie-clicker.
-A minimal Cookie Clicker made for the iPads in schools. This was coded for fun and isn't really good. 
+A minimal Cookie Clicker made for the iPads in schools. This was coded for fun and isn't really good. This was created used Codesandbox in school, with the badest keyboard possible. 
+
+How to play: go on the [website](https://cookie.henri.gg) => https://cookie.henri.gg and play.
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 import { useState } from "react";
+import { motion } from "framer-motion";
 export default function Home() {
   // insert a number, where the cookie-clicker is counting on
   const [click, setClick] = useState<number>(0);
   return (
-    <div className="min-h-screen w-full flex flex-col items-center justify-center">
-      <p>{click}</p>
+    <div className="min-h-screen w-full flex flex-col items-center justify-center dark:bg-black">
+      <button className="outline-none border-none">
+
+      </button>
+      <p className="text-black dark:text-white">{click}</p>
       <button
-        className="bg-black rounded-md p-5 text-white m-5"
+        className="bg-black rounded-md p-3 text-white m-5 dark:bg-white dark:text-black"
         onClick={() => setClick(click + 1)}
       >
         count
       </button>
+      <a href="https://github.com/i-am-henri/cookie-clicker" className="absolute bottom-2 right-2 underline dark:text-white" target="_blank">see github</a>
     </div>
   );
 }
