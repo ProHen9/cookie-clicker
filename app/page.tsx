1676cfe43cc1c4e8d12b6dd4cb50ff35b124c2c5
Henri <henri.me@skiff.com>
2023-12-09 17:23:03 +0100

    create small button theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,24 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 export default function Home() {
+  const [ButtonPositionSmall, setButtonPositionSmall] = useState<boolean>(false)
   // insert a number, where the cookie-clicker is counting on
   const [click, setClick] = useState<number>(0);
   return (
     <div className="min-h-screen w-full flex flex-col items-center justify-center dark:bg-black">
-      <button className="outline-none border-none">
-
-      </button>
-      <p className="text-black dark:text-white">{click}</p>
-      <button
-        className="bg-black rounded-md p-3 text-white m-5 dark:bg-white dark:text-black"
+      <motion.p initial={{opacity: 0}} animate={{opacity: 1}} onClick={() => setButtonPositionSmall(!ButtonPositionSmall)} className="absolute top-2 right-2 dark:text-white underline cursor-pointer" >change button theme</motion.p>
+      <motion.p initial={{opacity: 0}} style={{ display: ButtonPositionSmall? "none": "block"}} animate={{opacity: 1}} className="text-black dark:text-white">{click}</motion.p>
+      <motion.button
+        className="bg-black rounded-md p-3 text-white m-5 dark:bg-white dark:text-black flex"
         onClick={() => setClick(click + 1)}
       >
-        count
-      </button>
-      <a href="https://github.com/i-am-henri/cookie-clicker" className="absolute bottom-2 right-2 underline dark:text-white" target="_blank">see github</a>
+        <p className=" text-white dark:text-black mr-3" style={{ display: ButtonPositionSmall? "block": "none"}}>{click}</p>
+        counting
+      </motion.button>
+      <motion.a
+      initial={{opacity: 0, x: "-50px"}}
+      animate={{x: 0, opacity: 1}}
+      href="https://github.com/i-am-henri/cookie-clicker" className="absolute bottom-2 right-2 underline dark:text-white" target="_blank">see github</motion.a>
     </div>
   );
 }
