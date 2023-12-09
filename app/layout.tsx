10376a0910614c38cb609ecf5886f43993d95bc0
Henri <henri.me@skiff.com>
2023-12-09 17:24:15 +0100

    change name

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "cookii",
+  title: "cookie",
   description: "A minimaliistic cookie clicker, easy customizable.",
 };
 
