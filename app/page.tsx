9f2f0abb48dc9aaebdeb6dd263f839d17e5568e4
henri <henri.me@skiff.com>
2024-02-15 21:08:39 +0100

    add list style

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { DatabaseRow } from "@/schema/types";
 import HeroSection from "./hero";
-import LocalStorageCheck from "./localStorageCheck";
 export default async function Home() {
   const dataRaw = await fetch(`https://v1.appbackend.io/v1/rows/${process.env.TABLE_ID}?api_key=${process.env.API_KEY}`, { cache: 'no-store' })
   const data: DatabaseRow = await dataRaw.json() as DatabaseRow
@@ -8,17 +7,15 @@ export default async function Home() {
   console.log(data)
   return (
     <div className="min-h-screen w-full flex flex-col items-center justify-center dark:bg-black">
-      <LocalStorageCheck />
       <HeroSection />
       <section className="flex flex-col items-center justify-center w-full min-h-screen">
-        <h2 className="text-black dark:text-white">leaderboard</h2>
-        <ol>
+        <h2 className="text-black dark:text-white text-4xl">leaderboard</h2>
+        <ol >
           {
             data.data?.map((data) => {
               return (
-                <li className="flex flex-col items-center justify-center bg-[#111011]  dark:bg-white p-3 rounded-md " key={data._id}>
-                  <h2 className="text-white dark:text-black">{data.name}</h2>
-                  <h2 className="text-white dark:text-black">{data.count}</h2>
+                <li className=" dark:text-white" key={data._id}>
+                  {data.name} : {data.count}
                 </li>
               )
             })
