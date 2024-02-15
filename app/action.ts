882517df23eb5c34b6a00bf10d2c5f5ed327d23c
henri <henri.me@skiff.com>
2024-02-15 21:13:38 +0100

    add first code for saving new scores

---
+++ app/action.ts
@@ -0,0 +1,3 @@
+"use server"
+async function SaveScore(score: number, user: string) {
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import { DatabaseRow } from "@/schema/types";
 import HeroSection from "./hero";
+import { cookies } from "next/headers";
 export default async function Home() {
+  const userId = cookies().get("userId")
+  if (!userId) {
+    // handle later, when the score will saved
+  }
   const dataRaw = await fetch(`https://v1.appbackend.io/v1/rows/${process.env.TABLE_ID}?api_key=${process.env.API_KEY}`, { cache: 'no-store' })
   const data: DatabaseRow = await dataRaw.json() as DatabaseRow
   data.data?.sort((a, b) => b.count - a.count);
