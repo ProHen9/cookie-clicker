24f5501bd518654622294ff551e41168320adfef
henri <henri.me@skiff.com>
2024-02-15 18:33:24 +0100

    add database btw try to add

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
-"use client"
-import { useState } from "react";
+"use server"
 import { motion } from "framer-motion";
+import HeroSection from "./hero";
 export default async function Home() {
   const data = await fetch(`https://v1.appbackend.io/v1/rows/${process.env.TABLE_ID}?api_key=${process.env.API_KEY}`)
-  const [ButtonPositionSmall, setButtonPositionSmall] = useState<boolean>(false)
-  // insert a number, where the cookie-clicker is counting on
-  const [click, setClick] = useState<number>(0); // <= change this number to cheat. You will start at a higher count.
+  console.log(data)
   return (
     <div className="min-h-screen w-full flex flex-col items-center justify-center dark:bg-black">
-      
+      <HeroSection />
       <section className="flex flex-col items-center justify-center w-full min-h-screen">
         <motion.h2 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} className="text-black dark:text-white">leaderboard</motion.h2>
         <ol>
