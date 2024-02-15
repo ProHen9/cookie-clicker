87865539be972ee64ce1e44d3696093bd8f13802
henri <henri.me@skiff.com>
2024-02-15 20:58:48 +0100

    add a design for the leader board

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
 import { DatabaseRow } from "@/schema/types";
 import HeroSection from "./hero";
+import LocalStorageCheck from "./localStorageCheck";
 export default async function Home() {
-  const dataRaw = await fetch(`https://v1.appbackend.io/v1/rows/${process.env.TABLE_ID}?api_key=${process.env.API_KEY}`)
+  const dataRaw = await fetch(`https://v1.appbackend.io/v1/rows/${process.env.TABLE_ID}?api_key=${process.env.API_KEY}`, {
+    next: { revalidate: 100 },
+  })
   const data: DatabaseRow = await dataRaw.json() as DatabaseRow
   console.log(data)
   return (
     <div className="min-h-screen w-full flex flex-col items-center justify-center dark:bg-black">
+      <LocalStorageCheck />
       <HeroSection />
       <section className="flex flex-col items-center justify-center w-full min-h-screen">
         <h2 className="text-black dark:text-white">leaderboard</h2>
         {
           data.data?.map((data) => {
             return (
-              <div className="flex flex-col items-center justify-center w-full min-h-screen" key={data._id}>
-                <h2 className="text-black dark:text-white">{data.name}</h2>
-                <h2 className="text-black dark:text-white">{data.count}</h2>
+              <div className="flex flex-col items-center justify-center bg-[#111011]  dark:bg-white p-3 rounded-md " key={data._id}>
+                <h2 className="text-white dark:text-black">{data.name}</h2>
+                <h2 className="text-white dark:text-black">{data.count}</h2>
               </div>
             )
           })
