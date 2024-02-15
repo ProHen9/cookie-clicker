e817b8e9fb0f5f22bee1c8d2368bbf9f4f9e110b
henri <henri.me@skiff.com>
2024-02-15 19:58:25 +0100

    try to fix errror

--- app/hero.tsx
+++ app/hero.tsx
@@ -1,5 +1,4 @@
 "use client"
-import {motion} from "framer-motion"
 import { useState } from "react"
 
 export default function HeroSection() {
@@ -8,19 +7,18 @@ export default function HeroSection() {
     const [click, setClick] = useState<number>(0); // <= change this number to cheat. You will start at a higher count.
     return (
         <section className="flex flex-col items-center justify-center w-full min-h-screen">
-            <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} onClick={() => setButtonPositionSmall(!ButtonPositionSmall)} className="absolute top-2 right-2 dark:text-white underline cursor-pointer" >change button theme</motion.p>
-            <motion.p initial={{ opacity: 0 }} style={{ display: ButtonPositionSmall ? "none" : "block" }} animate={{ opacity: 1 }} className="text-black dark:text-white">{click}</motion.p>
-            <motion.button
+            <p  onClick={() => setButtonPositionSmall(!ButtonPositionSmall)} className="absolute top-2 right-2 dark:text-white underline cursor-pointer" >change button theme</p>
+            <p style={{ display: ButtonPositionSmall ? "none" : "block" }}  className="text-black dark:text-white">{click}</p>
+            <button
                 className="bg-black rounded-md p-3 text-white m-5 dark:bg-white dark:text-black flex"
                 onClick={() => setClick(click + 1)}
             >
                 <p className=" text-white dark:text-black mr-3" style={{ display: ButtonPositionSmall ? "block" : "none" }}>{click}</p>
                 counting
-            </motion.button>
-            <motion.a
-                initial={{ opacity: 0, x: "-50px" }}
-                animate={{ x: 0, opacity: 1 }}
-                href="https://github.com/i-am-henri/cookie-clicker" className="absolute bottom-2 right-2 underline dark:text-white" target="_blank">see github</motion.a>
+            </button>
+            <a
+                
+                href="https://github.com/i-am-henri/cookie-clicker" className="absolute bottom-2 right-2 underline dark:text-white" target="_blank">see github</a>
         </section>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use server"
-import { motion } from "framer-motion";
 import HeroSection from "./hero";
 export default async function Home() {
   const data = await fetch(`https://v1.appbackend.io/v1/rows/${process.env.TABLE_ID}?api_key=${process.env.API_KEY}`)
@@ -8,7 +7,7 @@ export default async function Home() {
     <div className="min-h-screen w-full flex flex-col items-center justify-center dark:bg-black">
       <HeroSection />
       <section className="flex flex-col items-center justify-center w-full min-h-screen">
-        <motion.h2 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} className="text-black dark:text-white">leaderboard</motion.h2>
+        <h2 className="text-black dark:text-white">leaderboard</h2>
         <ol>
 
         </ol>
