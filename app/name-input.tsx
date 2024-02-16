c5b5d308a0e9c8a13b7e902f858fa508d8f73963
henri <henri.me@skiff.com>
2024-02-16 21:27:03 +0100

    add a name input

--- app/name-input.tsx
+++ app/name-input.tsx
@@ -3,6 +3,6 @@ interface Props {
 }
 export default function NameInput(props: Props) {
     return (
-        <input placeholder={props.name || "you name"} className="outline-offset-2 p-2 rounded-[10px] dark:outline-white outline-dashed border-none" />
+        <input placeholder={props.name || "you name"} className="outline-offset-2 absolute top-[30px] left-[30px] outline-none focus:outline-2 p-2 rounded-[10px] focus:dark:outline-white  border-none" />
     )
 }
