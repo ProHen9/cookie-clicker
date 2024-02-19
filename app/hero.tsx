e767fdbf396825275f4c0bcc82175b55868367ee
henri <henri.me@skiff.com>
2024-02-19 19:36:12 +0100

    remove marker bug

--- app/hero.tsx
+++ app/hero.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useEffect, useState } from "react"
 import { SaveScore } from "./action";
+import { Button } from "@/components/ui/button";
 interface Props {
     count: number;
     user: string | undefined;
@@ -21,13 +22,13 @@ export default function HeroSection(props: Props) {
         <section className="flex flex-col items-center justify-center w-full min-h-screen">
             <p  onClick={() => setButtonPositionSmall(!ButtonPositionSmall)} className="absolute top-2 right-2 dark:text-white underline cursor-pointer" >change button theme</p>
             <p style={{ display: ButtonPositionSmall ? "none" : "block" }}  className="text-black dark:text-white">{click}</p>
-            <button
+            <Button
                 className="bg-black rounded-md p-3 text-white m-5 dark:bg-white dark:text-black flex"
                 onClick={() => setClick(click + 1)}
             >
                 <p className=" text-white dark:text-black mr-3" style={{ display: ButtonPositionSmall ? "block" : "none" }}>{click}</p>
                 counting
-            </button>
+            </Button>
             <a
                 
                 href="https://github.com/i-am-henri/cookie-clicker" className="absolute bottom-2 right-2 underline dark:text-white" target="_blank">see github</a>
